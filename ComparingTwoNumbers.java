500c3e888c3fd7d43a4ca48bea778df266b63d99
Andrey Bayshev <bayshev21@gmail.com>
2019-04-03 12:26:13 +0300

    Comparing Two Numbers

---
+++ ComparingTwoNumbers.java
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+
+public class ComparingTwoNumbers {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Scanner a = new Scanner(System.in);
+        do {
+            System.out.println("Введите поочередно несколько чисел:");
+            s.hasNextInt();
+            a.hasNextInt();
+            int number1 = s.nextInt();
+            int number2 = a.nextInt();
+            if (number1 > number2) {
+                System.out.println("number1 > number2");
+            } else {
+                System.out.println("number1 < number2");
+            }
+        }
+        while (true);
+    }
+}
